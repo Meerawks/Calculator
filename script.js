b0e4ff3bf0b2f86571a5c61ec57840cad03d06dc
meerawks <sameer.asif.mughal@gmail.com>
2024-02-26 21:42:33 +0500

    added UI to the calculator

--- script.js
+++ script.js
@@ -3,35 +3,142 @@ let buttonPressed=null;
 let operandFirst="";
 let operandSecond="";
 let operator="";
+let equation=[];
+let input="";
+let equationEvaluate=false;
+let operators=['x','+','-','÷','%','='];
 
 const output=document.querySelector('#display-text');
+const secondOutput=document.querySelector('#secondary-text');
 
 buttons.forEach(button=>{
     button.addEventListener('click',function getEventText(event){
         buttonPressed=event.target.innerText;
-        checkInput(buttonPressed);
-        console.log(buttonPressed);
+        if(buttonPressed=='AC'){
+            equation.length=0;
+            input='';
+            secondOutput.textContent="";
+            output.textContent="";
+        }else if (buttonPressed=='DEL'){
+            input = input.slice(0, -1);
+            output.textContent=input;
+        }
+        else if(operators.includes(buttonPressed)){
+            let secondarytext=secondOutput.innerText;
+            console.log(secondarytext);
+        
+    
+            if(buttonPressed=='='){
+                if(equation.length>1){
+                 console.table(equation);
+                 equation.push(input);
+                 console.table(equation);
+                console.table(equation);
+                secondOutput.textContent=equation.join("");
+                input="";
+                evaluate(equation);
+                console.table(equation);
+                }
+            }
+            
+            else if(operators.includes(secondarytext.charAt(secondarytext.length-1))&&input==''){
+                console.log('already operator added')
+            
+            }
+            else if(equation.length>1){
+                console.table(equation);
+                equation.push(input);
+                console.table(equation);
+                equation.push(buttonPressed);
+                console.table(equation);
+                secondOutput.textContent=equation.join("");
+                evaluate(equation);
+                equation.push(input);
+                equation.push(buttonPressed);
+                console.table(equation);
+                secondOutput.textContent=equation.join("")
+                console.table(equation);
+                input="";
+                output.textContent="";
+            }
+            else{
+               
+                if(equation[0]!=''){
+                    equation.push(input);
+                    console.table(equation);
+                equation.push(buttonPressed);
+                secondOutput.textContent=equation.join("");
+             output.textContent='';
+             input="";
+               console.table(equation);
+                }
+                else
+                equation.length=0;
+            }
+        
+             
+        }
+        else {
+            output.textContent='';
+            input+=buttonPressed;
+            let addComma=false;
+           for(let i=1;i<=input.length;i++){
+            
+            output.textContent=output.innerText;
+            if(addComma==true){
+                output.textContent+=',';
+                addComma=false;
+            }
+            output.textContent+=input[i-1];
+            console.log("i "+i)
+            if(i%3==0&&i!=0){
+                addComma=true;
+            }
+    
+           
+        }
+
+        }
     })
 })
-function checkInput(num){
-    let operators=['+', '-', 'X','÷'];
-    if(operators.includes(num)){
-        console.log('operator detected');
-      operator+=num; 
-      output.textContent+=operator; 
-    }
-    
-    if(num=='.'){
-        operandFirst=num;
-        output.textContent+=operandFirst;
-        console.log('. detected');
-    }
-    num=parseInt(num);
-    if(num>=0&&num<=9){
-        operandFirst=num;
-        console.log(operandFirst);
-        output.textContent+=operandFirst;
-        console.log('integer detected');
-    }
-   
+function evaluate(arr){
+    let operator=equation[1];
+  
+    let operands=equation.filter((value)=>equation.indexOf(value)%2==0);
+    console.table(operands);
+    operands=operands.map((value)=>parseFloat(value));
+    console.table(operands);
+    let result=0;
+    switch(operator) {
+        case 'x':
+          result=operands.reduce((total,num)=>total*num);
+          break;
+        case '+':
+            result=operands.reduce((total,num)=>total+num);
+          break;
+        case '-':
+            result=operands.reduce((total,num)=>total-num);
+          break;
+        case '%':
+            result=operands.reduce((total,num)=>total%num);
+          break;
+        case '÷':
+            result=operands.reduce((total,num)=>total/num);
+          break;
+        default:
+          // code block
+      }
+      if(isNaN(result)==false){
+      output.textContent=result;
+      input=result.toString();
+      equation.length=0;
+      console.table(equation);
+      }
+      else{
+        equation.pop();
+      }
+
 }
+
+   
+
